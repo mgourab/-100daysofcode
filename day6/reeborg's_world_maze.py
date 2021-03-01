2ab6371903a109eb7b62ad22bcfab7dab75b9983
mgourab <mgourabbam@gmail.com>
2021-03-01 10:06:21 +0530

    maze_reeborg's_world

---
+++ day6/reeborg's_world_maze.py
@@ -0,0 +1,15 @@
+# this is for the maze problem in reeborg's world site
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
