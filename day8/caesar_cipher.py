40182ed8423c54dcad1245de9db099b5f6038a24
mgourab <mgourabbam@gmail.com>
2021-03-14 11:52:51 +0530

    caesar_cipher_single_function

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -8,28 +8,16 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# create a function called 'encrypt' that takes the 'text' and 'shift' as inputs
-def encrypt(plain_text, shift_amount):
-    cipher_text = ''
-    for letter in plain_text:
+# create a function called caesar which can do both encode and decode also will take shift,direction as input..
+def caesar(cipher_text, shift_amount, cipher_direction):
+    final_text = ''
+    for letter in cipher_text:
         position = alphabet.index(letter)
+        if cipher_direction == 'decode':
+            shift_amount *= -1
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        final_text += alphabet[new_position]
+    print(f'The {cipher_direction}d text is: {final_text}')
 
 
-# Create a function called 'decrypt' that takes the 'text' and 'shift' as inputs
-def decrypt(text_to_decrypt, shift_to_decrypt):
-    decrypted_cipher = ''
-    for letter in text_to_decrypt:
-        position = alphabet.index(letter)
-        new_position = position - shift_to_decrypt
-        decrypted_cipher += alphabet[new_position]
-    print(f"The decoded text is {decrypted_cipher}")
-
-
-if direction == 'encode':
-    encrypt(plain_text=text, shift_amount=shift)
-else:
-    decrypt(text_to_decrypt=text, shift_to_decrypt=shift)
+caesar(cipher_text=text, shift_amount=shift, cipher_direction=direction)
